0907f3856f54914ddce0e975c61fa0cfd7ee7432
Wilhelm Gusztav M. Francia <wfrancia@stu.jsu.edu>
2018-02-12 17:51:11 -0600

    Finished part 2

--- src/main/java/edu/jsu/mcis/TicTacToe.java
+++ src/main/java/edu/jsu/mcis/TicTacToe.java
@@ -1,16 +1,33 @@
 package edu.jsu.mcis;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class TicTacToe {
 
     private static final int DEFAULT_WIDTH = 3;
+	
+	/*private static void GUI(){
+			JFrame frame = new JFrame("Tic-Tac-Toe");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		
+	}*/
 
     public static void main(String[] args) {
         
         /* Set initial size of game board (default is 3x3) */
 
         int width = DEFAULT_WIDTH;
-        
         /* If a different size is provided as a command-line argument, use it instead */
+		/*javax.swing.SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				GUI();
+			}
+		});
+		*/
+		
 
         if(args.length >= 1) {
             
@@ -25,16 +42,28 @@ public class TicTacToe {
 
         TicTacToeModel model = new TicTacToeModel(width);
         TicTacToeView view = new TicTacToeView(model);
-        TicTacToeController controller = new TicTacToeController(model, view);
+        //TicTacToeController controller = new TicTacToeController(model, view);
+		
+		
+		JFrame win = new JFrame("Tic-Tac-Toe");
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		win.add(view);
+		win.pack();
+		win.setVisible(true);
         
         /* MAIN LOOP */
+		
+		
 
-        while (!model.isGameover()){
+        /*while (!model.isGameover()){
             
             view.viewModel();
-            controller.controlModel();
+			
+            //controller.controlModel();
+			
             
         }
+		*/
         
         /* Game is over; show the final board and the winner */
 
--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -1,29 +1,55 @@
 package edu.jsu.mcis;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class TicTacToeView {
+public class TicTacToeView extends JPanel implements ActionListener{
 
     private TicTacToeModel model;
+	JPanel squaresPanel;
+	JButton[][] squares;
+	JLabel resultLabel;
     
     /* CONSTRUCTOR */
 	
     public TicTacToeView(TicTacToeModel model) {
         
         this.model = model;
-        
-    }
+		setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
+		squaresPanel = new JPanel(new GridLayout(model.getWidth(), model.getWidth()));
+		
+		squares = new JButton[model.getWidth()][model.getWidth()];
+		for(int row = 0; row < model.getWidth(); row++){
+			for(int col = 0; col < model.getWidth(); col++){
+				squares[row][col] = new JButton();
+				squares[row][col].setPreferredSize(new Dimension(64,64));
+				squares[row][col].addActionListener(this);
+				squares[row][col].setName("Square" + row + col);
+				//squares[row][col].add(squaresPanel);
+				squaresPanel.add(squares[row][col]);
+			}
+		}
+		resultLabel = new JLabel();
+		resultLabel.setName("ResultLabel");
+		resultLabel.setText("Hello");
+		
+        add(squaresPanel);
+		add(resultLabel);
+	}
 	
     public void viewModel() {
         
         /* Print the board to the console (see examples) */
         
         /* INSERT YOUR CODE HERE */
-		System.out.println("\n  012");
+		//System.out.println("\n  012");
         for(int i = 0; i < model.getWidth(); i++){
-            System.out.println("");
-            System.out.print(i + " ");
+            //System.out.println("");
+            //System.out.print(i + " ");
             for (int j = 0; j < model.getWidth(); j++){
-                System.out.print(model.getMark(i, j));
-            }
+                //System.out.print(model.getMark(i, j));
+				squares[i][j].setText(model.getMark(i,j).toString());
+			}
         }
 
     }
@@ -34,11 +60,11 @@ public class TicTacToeView {
 
         /* INSERT YOUR CODE HERE */
 		if (model.isXTurn())
-            System.out.println("\n\nPlayer 1 (X) Move: ");
+            resultLabel.setText("Player 1 (x) Move");
         else
-            System.out.println("\n\nPlayer 2 (O) Move: ");
+            resultLabel.setText("\n\nPlayer 2 (O) Move: ");
         
-        System.out.print("Enter the row and column numbers, separated by a space: ");
+        //System.out.print("Enter the row and column numbers, separated by a space: ");
 
     }
 
@@ -55,8 +81,41 @@ public class TicTacToeView {
 
         /* Display final winner */
 
-        System.out.println("\n\n" + r + "!");
+        //System.out.println("\n\n" + r + "!");
+		resultLabel.setText("\n\n" + r + "!");
 
     }
+
+    public void actionPerformed(ActionEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		/*
+		if (model.makeMark(row,col)){
+			System.out.println("");
+			model.makeMark(row, col);
+		}
+		else
+			view.showInputError();
+		*/
+		//while(!model.isGameover()){
+		for (int i = 0; i < model.getWidth(); i++){
+			for(int j = 0; j < model.getWidth(); j++){
+				if(e.getSource() == squares[i][j] && model.makeMark(i,j)){
+					squares[i][j].setText(model.getMark(i,j).toString());
+				}
+			}
+		}
+		if(model.getResult() == TicTacToeModel.Result.X){
+			resultLabel.setText("X");
+		}
+		
+		if(model.getResult() == TicTacToeModel.Result.O){
+			resultLabel.setText("O");
+		}
+		if(model.getResult() == TicTacToeModel.Result.TIE){
+			resultLabel.setText("TIE");
+		}
+		//}
+		
+	}
 	
 }
